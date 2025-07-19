c20aa7cf64b3fea4112293ba761e665c55b8a3d5
Gustavo Marques <gustavohnmarques@gmail.com>
2025-07-19 00:09:07 -0300

    style: padding changed

--- src/screens/Crypto/CryptoScreen.styles.ts
+++ src/screens/Crypto/CryptoScreen.styles.ts
@@ -3,7 +3,7 @@ import styled from '@emotion/native'
 export const Container = styled.SafeAreaView`
     flex: 1;
     background-color: #242c35;
-    padding: 10px;
+    padding: 0 10px;
 `
 
 export const ExchangeList = styled.ScrollView`
