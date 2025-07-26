efe84a4950e062a7a9c27882c2de903f0dcd5f7c
Gustavo Marques <gustavohnmarques@gmail.com>
2025-07-25 22:42:00 -0300

    feat: custom toast component

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import BottomTabs from './navigation/BottomTabs';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { SafeAreaView } from 'react-native';
+import CustomToast from './components/CustomToast/CustomToast';
+
 const App = () => {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <BottomSheetModalProvider>
-        <SafeAreaView style={{flex: 1}}>
-        <BottomTabs />
+        <SafeAreaView style={{ flex: 1 }}>
+          <BottomTabs />
+          <CustomToast />
         </SafeAreaView>
       </BottomSheetModalProvider>
     </GestureHandlerRootView>
